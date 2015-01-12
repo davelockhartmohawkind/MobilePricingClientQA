5a9e015ee5fe668807037ec58684f7140c89f2e1
davelockhartmohawkind <dave_lockhart@mohawkind.com>
2015-01-12 11:27:34 -0500

    v64

--- startup.js
+++ startup.js
@@ -8,6 +8,7 @@ var cper = 0;
 var cTxt = 'Untested';
 var oldstate = false;
 var myInterval = null;
+var checkingURL = false;
 
 
 
@@ -18,15 +19,29 @@ $(document).ready(function () {
     $('.indicator').css('display', 'block');
     $('#test_signal').delay(600).fadeIn(600);
 
+    //window.plugin.email.open({
+    //    to: ['max.mustermann@appplant.de'],
+    //    cc: ['erika.mustermann@appplant.de'],
+    //    bcc: ['john.doe@appplant.com', 'jane.doe@appplant.com'],
+    //    subject: 'Greetings',
+    //    body: 'How are you? Nice greetings from Leipzig'
+    //});
+
     var handleExists = function (exists) {
+        checkingURL = false;
         //do more stuff based on the boolean value of exists
         if (exists) {
 
-            setTimeout(clearInterval(myInterval),1000);
-            window.location.href = "http://mobilepricingqa.mohawkind.com";
+            try{
+                setTimeout(clearInterval(myInterval), 500);
+                window.location.href = "Main.html";
+            }
+            catch (err)
+            {
 
-            // var ref = window.open('http://mobilepricingqa.mohawkind.com', '_self', 'toolbar=no,location=no');
-            //window.plugins.ChildBrowser.showWebPage('http://mobilepricingqa.mohawkind.com/Home/Login',
+            }
+            // var ref = window.open('http://mobilepricingdev.mohawkind.com', '_self', 'toolbar=no,location=no');
+            //window.plugins.ChildBrowser.showWebPage('http://mobilepricingdev.mohawkind.com',
             //                        {
             //                            showLocationBar: false,
             //                            showNavigationBar: false,
@@ -34,50 +49,58 @@ $(document).ready(function () {
             //                        });
            
         }
-        else {
+        else
+        {
 
             $('#loadmsg').html('Internet Destination Is Not Available.');
+            $("#try_again").css('display', 'block');
         }
     }
 
 
     function urlExists(url) {
-        $.ajax({
-            type: 'HEAD',
-            url: url,
-            success: function () {
-                handleExists(true);
-            },
-            error: function () {
-                handleExists(false);
-            }
-        });
+        if (checkingURL == false) {
+            checkingURL = true;
+            $.ajax({
+                type: 'HEAD',
+                url: url,
+                success: function () {
+                    handleExists(true);
+                },
+                error: function () {
+                    handleExists(false);
+                }
+            });
+        }
+        else
+        {
+            //skip it this time
+        }
     }
 
+    
+    $('#try_again').css('display', 'none');
 
     var NetworkUpCounter = 0;
 
-   
     myInterval = setInterval(function () {
-
         oldState = navigator.onLine ? 'online' : 'offline';
         if (oldState == "offline") {
             NetworkUpCounter = 0;
             $('#loadmsg').html('Network Connection Down.');
         }
         else {
-           
+
             NetworkUpCounter += 1;
             //skip the first time through to give network time to settle;
             if (NetworkUpCounter > 1) {
                 $('#loadmsg').html('Network Ready.');
-               // clearInterval(myInterval);
+                // clearInterval(myInterval);
                 urlExists('http://mobilepricingqa.mohawkind.com');
-               
+
             }
         }
     }, 1250);
-
 
 });
 // Document.ready: END
